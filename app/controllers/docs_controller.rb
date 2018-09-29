47fa632bd1b711eb10fe6b6f2285d34fa8193afe
Mileidy Tacuri <mileidy.tacuri@pcgconsultinggroup.com>
2018-09-29 15:30:52 -0500

    adding docs views and actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,12 +1,16 @@
 class DocsController < ApplicationController
-
+	before_action :find_doc, only:[:show, :edit, :update, :destroy]
+	
 	def index
+		@docs = Doc.all.order("created_at DESC")
 	end
 
 	def new
+		@doc = Doc.new
 	end
 
 	def show
+
 	end
 
 	def edit
@@ -19,13 +23,22 @@ class DocsController < ApplicationController
 	end
 
 	def create
+		@doc = Doc.new(doc_params)
+
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 	end
 
 	private
 
 		def find_doc
+			@doc = 	Doc.find(paarams[:id])
 		end
 
 		def doc_params
+			paarams.require(:doc).permit(:title, :content)
 		end
 end
